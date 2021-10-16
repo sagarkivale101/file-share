28449d3448a13ff8330b45a36c204601d4a642ca
sagarkivale101 <sagarkivale101@gmail..com>
2021-10-16 09:28:52 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+function connectDB() {
+    // Database connection 🥳
+    mongoose.connect(process.env.MONGO_CONNECTION_URL);
+    
+    //mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('Database connected 🥳🥳🥳🥳');
+    }).on('error', function (err) {
+        console.log(err);
+      });
+}
+
+
+
+// mIAY0a6u1ByJsWWZ
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+//console.log(__dirname);
+app.use(express.json());
+const connectDB = require('./config/db.js');
+connectDB();
+//template engine is
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+//routes
+
+app.use('/api/files', require('./routes/files.js'));
+
+app.use('/files', require('./routes/show.js'));
+app.use('/files/download', require('./routes/download'));
+
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
